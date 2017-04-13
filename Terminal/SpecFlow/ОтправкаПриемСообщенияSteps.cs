b9bd59d17bcb29f0b6e8782718d8e31f088df370
medvedevvo <vladrobot@mail.ru>
2017-04-13 14:37:49 +0400

    Terminal added

---
+++ Terminal/SpecFlow/ОтправкаПриемСообщенияSteps.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminal;
+
+namespace SpecFlow
+{
+    [Binding]
+    public class ОтправкаПриемСообщенияSteps
+    {
+        private static COMPort com1, com2;
+        private string msg;
+        private static bool is_recieved = false;
+
+        [Given(@"порт ""(.*)"" и порт ""(.*)"" с частотой (.*) открыты")]
+        public void ДопустимПортИПортСЧастотойОткрыты(string port_name1, string port_name2, int port_rate)
+        {
+            com1 = new COMPort(port_name1, port_rate);
+            com1.open();
+
+            com2 = new COMPort(port_name2, port_rate);
+            com2.open();
+            com2.onRecieve += recieve_msg;
+            Assert.AreEqual(true, com1.state, "com_1 not open");
+            Assert.AreEqual(true, com2.state, "com_2 not open");
+        }
+        
+        [Given(@"я ввел сообщение ""(.*)""")]
+        public void ДопустимЯВвелСообщение(string msg)
+        {
+            this.msg = msg;
+        }
+        
+        [When(@"я нажимаю кнопку Отправить")]
+        public void ЕслиЯНажимаюКнопкуОтправить()
+        {
+            com1.send(msg);
+        }
+
+        [Then(@"я жду")]
+        public void ЯЖду()
+        {
+            int i = 0;
+            while (!is_recieved && (i < 1000000)) { i++; }
+        }
+
+        [Then(@"сообщение отправляется")]
+        public void ТоСообщениеОтправляется()
+        {
+            Assert.AreEqual(true, is_recieved, "Not recieved");
+            com1.close();
+            com2.close();
+        }
+
+        private void recieve_msg(string msg)
+        {
+            is_recieved = true;
+        }
+    }
+}
---
+++ Terminal/SpecFlow/ОтправкаСообщенияСЗакрытымПортомSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terminal;
+
+namespace SpecFlow
+{
+    [Binding]
+    public class ОтправкаСообщенияСЗакрытымПортомSteps
+    {
+        private static COMPort com;
+        private string msg;
+
+        [Given(@"я хочу отправить сообщение ""(.*)""")]
+        public void ДопустимЯХочуОтправитьСообщение(string msg)
+        {
+            com = new COMPort("COM28", 115200);
+            this.msg = msg;
+        }
+        
+        [When(@"я нажимаю на кнопку Отправить")]
+        public void ЕслиЯНажимаюНаКнопкуОтправить()
+        {
+            com.send(msg);
+        }
+        
+        [Then(@"ошибка, порт закрыт")]
+        public void ТоОшибкаПортЗакрыт()
+        {
+            Assert.AreEqual(false, com.state, "Port is opened");
+        }
+    }
+}
