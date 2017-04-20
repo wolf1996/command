e7e16746ba36d63806c57b0c8a8f2ab73a2ce557
Vladimir Rasskazov <vr.rasskazov@gmail.com>
2017-04-20 17:12:53 +0300

    form template and random answer added

--- CarTerminal_V1.0/Terminal/CommandTest.cs
+++ CarTerminal_V1.0/Terminal/CommandTest.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace Terminal
 {
-    public class CommandTest
+    public static class CommandTest
     {
-        public string ReceiveCmd(string msg)
+        static Random rand = new Random(DateTime.Now.Millisecond);
+        static double last_time = 0;
+        /// <returns>ответ на сообщение</returns>
+        public static string AnswerToCmd(string msg, double time_expectation, int current_mode_number) 
         {
             string[] cmd = msg.Split(':');
-            string[] parameters;
-            string answer;
+            string answer = "";
 
             switch (cmd[0])
             {
@@ -41,7 +43,14 @@ namespace Terminal
                     answer = "";
                     break;
                 case "@START": // ответ IMU7/ENC3 в автоматическом режиме
-                    answer = "@START_APPLY";
+                    if (current_mode_number == 0)
+                    {
+                        answer = "@START_APPLY";
+                    }
+                    else
+                    {
+                        answer = AutoGenCmd();
+                    }
                     break;
                 case "@STOP":
                     answer = "@STOP_APPLY";
@@ -66,16 +75,23 @@ namespace Terminal
                         answer = "";
                     }
                     break;
+                case "@SEMI3": // @SEMI2:mid_speed;a;freq
+                    if (time_expectation > 300)
+                    {
+                        answer = "@STOP";
+                    }
+                    else
+                    {
+                        answer = "@SEMI_APPLY";
+                    }
+                    break;
                 case "@AUTO_INIT6": // @AUTO_INIT6: freq; weight; coe; imp; radius_static; radius_dynamic
                     answer = "@AUTO_INIT_APPLY";
                     break;
                 case "IMU7": // IMU7:time;axel_x;axel_y;axel_z;gyro_x;gyro_y;gyro_z
-                    // parameters = msg.Split(';');
-                    // ответ всегда пустой?
                     answer = "";
                     break;
                 case "ENC3": // ENC3:time;enc_l;enc_r
-                    // ответ всегда пустой?
                     answer = "";
                     break;
 
@@ -85,6 +101,29 @@ namespace Terminal
             }
 
             return answer;
+        }
+
+        private static string AutoGenCmd()
+        {
+            int n_rand_params;
+            string cmd = "";
+            if (rand.Next(0, 1) == 0)
+            {
+                n_rand_params = 6;
+                cmd += "@IMU7:";
+            }
+            else
+            {
+                n_rand_params = 2;
+                cmd += "ENC3:";
+            }
+            last_time += rand.NextDouble();
+            cmd += last_time;
+            for (int i = 0; i < n_rand_params; i++)
+            {
+                cmd += ";" + rand.NextDouble();
+            }
+            return cmd;
         }
     }
 }
